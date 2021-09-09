c5d6fada96597a125b0bcd19329dbc6cc67d8397
Esraa Azab <sara2_fun2010@yahoo.com>
2021-09-09 18:57:20 +0200

    Complete ingredientListDetailsViewController View

--- NATask/Scenes/IngredientListDetails/View/IngredientListDetailsViewController.swift
+++ NATask/Scenes/IngredientListDetails/View/IngredientListDetailsViewController.swift
@@ -6,24 +6,49 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class IngredientListDetailsViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    @IBOutlet weak var ingredientTableView: UITableView!
+    @IBOutlet weak var totalNutritionButton: UIButton!
+    
+    // MARK: - Properties
+    var ingredientLisrDetailsViewModal: IngredientLisrDetailsViewModal!
+    private let disposeBag = DisposeBag()
+    private let ingredientCellIdentifierAndNibName = "IngredientTableViewCell"
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTabelViewCell()
+        bind()
+        ingredientLisrDetailsViewModal.ingredientsSubjectOnNext()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupTabelViewCell() {
+        ingredientTableView.register(UINib(nibName: ingredientCellIdentifierAndNibName, bundle: .main), forCellReuseIdentifier: ingredientCellIdentifierAndNibName)
     }
-    */
-
+    
+    // MARK: - Bind Method
+    func bind() {
+        totalNutritionButton.rx.tap
+            .subscribe(onNext: { [weak self](_) in
+                guard let self = self else { return }
+                self.ingredientLisrDetailsViewModal.showTotalNutrition(view: self)
+            }).disposed(by: disposeBag)
+        
+        ingredientLisrDetailsViewModal.ingredientModelObservable
+            .bind(to: self.ingredientTableView
+                    .rx
+                    .items(cellIdentifier: ingredientCellIdentifierAndNibName,
+                           cellType: IngredientTableViewCell.self)) { row, ingredient, cell in
+                cell.caloriesLabel.text = "\(ingredient.calories ?? 0)"
+                cell.quantityUnitAndFoodLabel.text = ingredient.ingredientText ?? ""
+                cell.weightLabel.text = "\(ingredient.totalWeight ?? 0)"
+            }
+            .disposed(by: disposeBag)
+    }
 }
--- NATask/Scenes/IngredientListDetails/ViewModal/IngredientListDetailsViewModal.swift
+++ NATask/Scenes/IngredientListDetails/ViewModal/IngredientListDetailsViewModal.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+final class IngredientLisrDetailsViewModal {
+    
+    // MARK: - Properties
+    let ingredients: [IngredientModal]?
+    private var ingredientsModelSubject = PublishSubject<[IngredientModal]>()
+    
+    var ingredientModelObservable: Observable<[IngredientModal]> {
+        return ingredientsModelSubject
+    }
+    
+    // MARK: - Initialize
+    init(ingredients: [IngredientModal]?) {
+        self.ingredients = ingredients
+    }
+    
+    func ingredientsSubjectOnNext() {
+        self.ingredientsModelSubject.onNext(ingredients ?? [])
+    }
+    
+    // MARK: - Navigate to Total Nutrition
+    func showTotalNutrition(view: UIViewController) {
+        
+    }
+}
