83007cc31b7261dba77e8db4440389ca956ddffe
Esraa Azab <sara2_fun2010@yahoo.com>
2021-09-09 21:41:49 +0200

    Enable IQKeyboard

--- NATask/AppDelegate/AppDelegate.swift
+++ NATask/AppDelegate/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        IQKeyboardManager.shared.enable = true
         // Override point for customization after application launch.
         return true
     }
