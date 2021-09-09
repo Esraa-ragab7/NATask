8fb42f9a903d9491cc73a26674db4cf987dea2b0
Esraa Azab <sara2_fun2010@yahoo.com>
2021-09-09 17:18:30 +0200

    Add ingredientTabelViewCell files

---
+++ NATask/Scenes/IngredientListDetails/Cells/IngredientTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  IngredientTableViewCell.swift
+//  NATask
+//
+//  Created by Esraa Mohamed Ragab on 09/09/2021.
+//
+
+import UIKit
+
+class IngredientTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
