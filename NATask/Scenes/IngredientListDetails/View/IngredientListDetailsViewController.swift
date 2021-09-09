f8364765413c94df2a9a2ecbc68f77cad4eaf703
Esraa Azab <sara2_fun2010@yahoo.com>
2021-09-09 16:03:25 +0200

    Add IngredientListDetailsViewController and view Modal

---
+++ NATask/Scenes/IngredientListDetails/View/IngredientListDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  IngredientListDetailsViewController.swift
+//  NATask
+//
+//  Created by Esraa Mohamed Ragab on 09/09/2021.
+//
+
+import UIKit
+
+class IngredientListDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
