d2c34424115a697877c731789928b1eb839f88b6
Esraa Azab <sara2_fun2010@yahoo.com>
2021-09-09 21:19:33 +0200

    Add facts tableviewcell and change storyboard place

---
+++ NATask/Scenes/TotalNutritionFacts/Cells/FactsTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  FactsTableViewCell.swift
+//  NATask
+//
+//  Created by Esraa Mohamed Ragab on 09/09/2021.
+//
+
+import UIKit
+
+class FactsTableViewCell: UITableViewCell {
+
+    // MARK: - Outlets
+    @IBOutlet weak var factTitleLabel: UILabel!
+    @IBOutlet weak var factValueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
---
+++ NATask/Scenes/TotalNutritionFacts/View/TotleNutritionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TotleNutritionViewController.swift
+//  NATask
+//
+//  Created by Esraa Mohamed Ragab on 09/09/2021.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class TotleNutritionViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var ingredientTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
