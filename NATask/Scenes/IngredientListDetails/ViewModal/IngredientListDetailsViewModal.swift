5848153f60b29308568747b917aef509c2bec717
Esraa Azab <sara2_fun2010@yahoo.com>
2021-09-09 21:20:48 +0200

    Navigate to Total nutrition screen

--- NATask/Scenes/IngredientListDetails/ViewModal/IngredientListDetailsViewModal.swift
+++ NATask/Scenes/IngredientListDetails/ViewModal/IngredientListDetailsViewModal.swift
@@ -30,6 +30,38 @@ final class IngredientLisrDetailsViewModal {
     
     // MARK: - Navigate to Total Nutrition
     func showTotalNutrition(view: UIViewController) {
+        let totleNutritionViewController = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "TotleNutritionViewController")
+        if let totleNutritionViewController = totleNutritionViewController as? TotleNutritionViewController {
+            totleNutritionViewController.totalNutritionViewModal = TotalNutritionViewModal(facts: getFacts())
+        }
+        totleNutritionViewController.modalPresentationStyle = .fullScreen
+        view.navigationController?.pushViewController(totleNutritionViewController, animated: true)
+    }
+
+    func getFacts() -> [Facts] {
+        let requiredCalculations = [
+            "Calories": 1,
+            "Fat": 1,
+            "Cholesterol": 1,
+            "Sodium": 1,
+            "Carbs": 1,
+            "Fiber": 1,
+            "Protein": 1,
+            "Vitamin": 1,
+            "Calcium": 1,
+            "Iron": 1,
+            "Potassium": 1
+        ]
+        var factsDic = [String: Facts]()
+        for item in ingredients ?? [] {
+            factsDic["Calories"] = Facts(label: "Calories", quantity: ((factsDic["Calories"]?.quantity ?? 0.0) + Double(item.calories ?? 0)), unit: "kcal")
+            for fact in item.totalDaily ?? [:] {
+                let prevValue = factsDic[fact.key]
+                let newQuantity = (prevValue?.quantity ?? 0) + (fact.value.quantity ?? 0)
+                factsDic[fact.key] = Facts(label: fact.value.label, quantity: newQuantity, unit: fact.value.unit)
+            }
+        }
         
+        return factsDic.map({return $0.value}).filter({requiredCalculations[$0.label ?? ""] == 1})
     }
 }
