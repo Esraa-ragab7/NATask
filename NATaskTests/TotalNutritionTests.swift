72e527948a6419583a17dbecc21da7861c0f52bd
Esraa Azab <sara2_fun2010@yahoo.com>
2021-09-10 07:55:31 +0200

    Complete TotalNutritionTests.

--- NATaskTests/TotalNutritionTests.swift
+++ NATaskTests/TotalNutritionTests.swift
@@ -6,27 +6,45 @@
 //
 
 import XCTest
+@testable import NATask
 
 class TotalNutritionTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+    var totleNutritionViewController: TotleNutritionViewController!
+    var value1 = "20"
+    var value2 = "11"
+    
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // MARK: - Case1 Success
+        initTotleNutritionViewController()
+        checkRound0f()
+        
+        // MARK: - Case2 Failure
+        /*
+            value1 = "21"
+            value2 = "10"
+            initTotleNutritionViewController()
+            checkRound0f()
+        */
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func initTotleNutritionViewController() {
+        totleNutritionViewController = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "TotleNutritionViewController") as? TotleNutritionViewController
+        if let totleNutritionViewController = totleNutritionViewController {
+            totleNutritionViewController.totalNutritionViewModal = TotalNutritionViewModal(facts: [
+                Facts(label: "test1", quantity: 20.37123123, unit: "%"),
+                Facts(label: "test2", quantity: 10.72132, unit: "%")
+            ])
         }
+    }
+    
+    func checkRound0f() {
+        let viewModal = totleNutritionViewController.totalNutritionViewModal
+        let facts = viewModal?.facts
+        let resValue1 = viewModal?.round0f(quantity: facts?[0].quantity ?? 0) ?? ""
+        XCTAssert(value1.elementsEqual(resValue1),"Value1 Not Equal Test1 Fact1")
+        let resValue2 = viewModal?.round0f(quantity: facts?[1].quantity ?? 0) ?? ""
+        XCTAssert(value2.elementsEqual(resValue2), "Value2 Not Equal Test2 Fact2")
     }
 
 }
