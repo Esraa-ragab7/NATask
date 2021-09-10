5ccc2c18721011876caf6206c4b8b1ed7ed00633
Esraa Azab <sara2_fun2010@yahoo.com>
2021-09-10 03:54:18 +0200

    Add isConnectedToInternet to the connectivity file.

--- NATask/NetworkLayer/Connnectivity.swift
+++ NATask/NetworkLayer/Connnectivity.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import Alamofire
+
+struct Connectivity {
+    static let sharedInstance = NetworkReachabilityManager()!
+    static var isConnectedToInternet:Bool {
+        return self.sharedInstance.isReachable
+    }
+}
