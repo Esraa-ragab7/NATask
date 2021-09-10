c8357ca68a3fb4e1b356094b4b142ce8154e6f6f
Esraa Azab <sara2_fun2010@yahoo.com>
2021-09-10 03:53:59 +0200

    Add alert method to UIViewController extension

--- NATask/Helpers/Extensions/UIViewController+Extension.swift
+++ NATask/Helpers/Extensions/UIViewController+Extension.swift
@@ -5,4 +5,13 @@
 //  Created by Esraa Mohamed Ragab on 10/09/2021.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func alert(message: String, title: String = "") {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(OKAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
